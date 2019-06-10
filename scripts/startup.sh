1745a8540980fe9a90c8678e1f4553dd66aa440d
nierdz <kevin@igln.fr>
2019-06-10 17:44:31 +0200

    chore: consul works in this state !

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,6 +5,9 @@ set -o pipefail
 set -o nounset
 
 /bin/systemctl stop consul.service
+#/bin/systemctl stop nomad.service
 rm -f /var/consul/node-id
+#rm -f /var/nomad/client/client-id
 
 su - admin -c "cd /home/admin; /home/admin/venv/bin/ansible-playbook -t consul playbook.yml"
+#su - admin -c "cd /home/admin; /home/admin/venv/bin/ansible-playbook -t nomad playbook.yml"
