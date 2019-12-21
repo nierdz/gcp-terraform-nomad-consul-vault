f19d9606415e9a2c11301da56737677d6add17c8
nierdz <kevin@igln.fr>
2019-12-21 17:41:40 +0100

    chore: only run ansible and start vault in startup script

--- scripts/startup-consul-vault.sh
+++ scripts/startup-consul-vault.sh
@@ -5,8 +5,8 @@ set -o pipefail
 set -o nounset
 
 /bin/systemctl stop consul.service
-/bin/systemctl stop vault.service
 rm -rf /var/consul/*
-rm -rf /var/vault/*
 
 su - admin -c "cd /home/admin/ansible; TAGS=consul,vault make -f ../Makefile run-ansible"
+
+/bin/systemctl start vault.service
