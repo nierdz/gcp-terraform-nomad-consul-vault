51a6de11c322f262e71b5e38a9890850aa143d6b
nierdz <kevin@igln.fr>
2019-12-23 22:19:50 +0100

    refactor: update env.sh to make it project name agnostic

--- env.sh
+++ env.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env/bash
 
-export GCE_PROJECT=terraform-nomad-consul-vault
-export GCE_PEM_FILE_PATH=~/.gcloud/terraform-nomad-consul-vault.json
+export GCE_PROJECT=your-project-name
+export GCE_PEM_FILE_PATH=~/.gcloud/your-project-name.json
 export CLOUDSDK_ACTIVE_CONFIG_NAME=$GCE_PROJECT
-export GOOGLE_APPLICATION_CREDENTIALS=~/.gcloud/terraform-nomad-consul-vault.json
+export GOOGLE_APPLICATION_CREDENTIALS=~/.gcloud/your-project-name.json
 export GOOGLE_PROJECT=$GCE_PROJECT
 GCE_EMAIL=$(grep client_email $GCE_PEM_FILE_PATH | grep -oE "[0-9a-z\\-]*@[0-9a-z\\-]*\\.iam\\.gserviceaccount\\.com")
 export GCE_EMAIL
