0c7e0bda1bb5243454529e1f8cba84c68ca6c917
nierdz <kevin@igln.fr>
2019-12-28 09:56:21 +0100

    feat: use my own consul role, much quicker and simple

--- scripts/startup-consul-vault.sh
+++ scripts/startup-consul-vault.sh
@@ -4,9 +4,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-/bin/systemctl stop consul.service
-rm -rf /var/consul/*
-
 su - admin -c "cd /home/admin/ansible; TAGS=consul,vault make -f ../Makefile run-ansible"
 
 /bin/systemctl start vault.service
+/bin/systemctl start consul.service
